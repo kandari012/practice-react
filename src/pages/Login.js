7913f190aa5a5199aea386a281e92e4f4ae8b529
RahulKandari12 <rahul.kandari@adglobal360.com>
2021-09-15 17:55:35 +0530

    login UI

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <form>
+      <h4>Login</h4>
+      <div>
+        <input type="email" placeholder="Email" required />
+      </div>
+      <br />
+      <div>
+        <input type="password" placeholder="Password" required />
+      </div>
+      <br />
+      <div>
+        <button>Log In</button>
+      </div>
+    </form>
+  );
 }
 
 export default Login;
